567d613e7cfc612409d4fe9568e42784c314ff40
DarkH0le <andresivan.aldama@gmail.com>
2018-03-09 20:23:40 -0600

    winner working

--- TRY1/WinnersView.swift
+++ TRY1/WinnersView.swift
@@ -17,7 +17,21 @@ class WinnersView: UIViewController {
         newlabel.translatesAutoresizingMaskIntoConstraints = false
         return newlabel
     }()
+    let playAgainBtn : UIButton = {
+       let btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Play Again" , for: .normal)
+        btn.backgroundColor = UIColor.cyan
+        btn.layer.cornerRadius = 3
+        btn.layer.shadowOpacity = 0.8
+        btn.layer.shadowOffset = CGSize(width: 3, height: 2)
+        btn.addTarget(self, action: #selector(showWinner), for: .touchUpInside)
+        return btn
+    }()
     
+    @objc func showWinner() -> Void{
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.brown
@@ -33,9 +47,16 @@ class WinnersView: UIViewController {
     func setViews() -> Void {
         //Adding views
         view.addSubview(labelWinner)
+        view.addSubview(playAgainBtn)
         //Set Contr
         labelWinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         labelWinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //Play Button
+        playAgainBtn.heightAnchor.constraint(greaterThanOrEqualToConstant: 36).isActive = true
+        playAgainBtn.widthAnchor.constraint(greaterThanOrEqualToConstant: 88).isActive = true
+        playAgainBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        playAgainBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        playAgainBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:-(view.bounds.height*1/10)).isActive = true
     }
 }
 
